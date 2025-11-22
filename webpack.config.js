aa7a1f66f8ac5a6f9759782ff03f32a597b535d9
star874 <stanleywestley0900@gmail.com>
2025-11-22 05:53:39 -0800

    Initial commit: AI Blocks visual programming app

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,11 +24,6 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ],
   resolve: {
     extensions: ['.js', '.jsx']
   }
